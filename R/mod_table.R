5882a08bef3395131a6a553538a5840072d41826
Christopher.Donovan <christopher.donovan@uvm.edu>
2024-10-22 17:32:21 -0400

    new data import

--- R/mod_table.R
+++ R/mod_table.R
@@ -53,11 +53,93 @@ mod_table_server <- function(id){
     output$metrics_table <- reactable::renderReactable({
       reactable(
         metadata,
+        sortable = TRUE,
+        resizable = TRUE,
+        filterable = TRUE,
+        searchable = TRUE,
+        pagination = TRUE,
+        bordered = TRUE,
+        wrap = TRUE,
+        rownames = FALSE,
+        onClick = 'select',
+        striped = TRUE,
+        pageSizeOptions = c(5, 10, 25, 50, 100),
+        defaultPageSize = 5,
+        showPageSizeOptions = TRUE,
+        highlight = TRUE,
+        style = list(fontSize = "14px"),
+        compact = TRUE,
         columns = list(
-          Metric = colDef(minWidth = 200),
-          Definition = colDef(minWidth = 250),
-          Year = colDef(minWidth = 75)
-        )
+          # Dimension = colDef(
+          # minWidth = 75,
+          # sticky = 'left'
+          # ),
+          # Index = colDef(
+          # minWidth = 75,
+          # sticky = 'left'
+          # ),
+          # Indicator = colDef(
+          # minWidth = 100,
+          # sticky = 'left'
+          # ),
+          Metric = colDef(
+            minWidth = 200,
+            sticky = 'left'
+          ),
+          Definition = colDef(
+            minWidth = 250,
+          ),
+          # Units = colDef(minWidth = 50),
+          Year = colDef(minWidth = 75),
+          Source = colDef(minWidth = 250),
+          Scope = colDef(show = FALSE),
+          Resolution = colDef(show = FALSE),
+          Url = colDef(
+            minWidth = 300,
+            show = FALSE
+          )
+        ),
+        defaultColDef = colDef(minWidth = 100),
+        # elementId = "metrics_table",
+        details = function(index) {
+          div(
+            style = "padding: 15px; border: 1px solid #ddd; margin: 10px 0;
+             background-color: #E0EEEE; border-radius: 10px; border-color: black;
+             box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);",
+            
+            tags$h4(
+              strong("Details"),
+            ),
+            tags$p(
+              strong('Metric Name: '),
+              as.character(metadata[index, 'Metric']),
+            ),
+            tags$p(
+              strong('Definition: '),
+              as.character(metadata[index, 'Definition']),
+            ),
+            tags$p(
+              strong('Source: '),
+              as.character(metadata[index, 'Source'])
+            ),
+            tags$p(
+              strong('Latest Year: '),
+              as.character(metadata[index, 'Year'])
+            ),
+            # tags$p(
+            #   strong('Updates: '),
+            #   str_to_title(as.character(metadata[index, 'Updates']))
+            # ),
+            tags$p(
+              strong('URL: '),
+              tags$a(
+                href = as.character(metadata[index, 'Url']),
+                target = '_blank',
+                as.character(metadata[index, 'Url'])
+              )
+            )
+          )
+        }
       )
     })
     
@@ -134,45 +216,45 @@ mod_table_server <- function(id){
     #       ),
     #       defaultColDef = colDef(minWidth = 100),
     #       elementId = "metrics_table",
-    #       details = function(index) {
-    #         div(
-    #           style = "padding: 15px; border: 1px solid #ddd; margin: 10px 0;
-    #          background-color: #E0EEEE; border-radius: 10px; border-color: black;
-    #          box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);",
-    # 
-    #           tags$h4(
-    #             strong("Details"),
-    #           ),
-    #           tags$p(
-    #             strong('Metric Name: '),
-    #             as.character(metadata[index, 'Metric']),
-    #           ),
-    #           tags$p(
-    #             strong('Definition: '),
-    #             as.character(metadata[index, 'Definition']),
-    #           ),
-    #           tags$p(
-    #             strong('Source: '),
-    #             as.character(metadata[index, 'Source'])
-    #           ),
-    #           tags$p(
-    #             strong('Latest Year: '),
-    #             as.character(metadata[index, 'Year'])
-    #           ),
-    #           # tags$p(
-    #           #   strong('Updates: '),
-    #           #   str_to_title(as.character(metadata[index, 'updates']))
-    #           # ),
-    #           tags$p(
-    #             strong('URL: '),
-    #             tags$a(
-    #               href = as.character(metadata[index, 'Url']),
-    #               target = '_blank',
-    #               as.character(metadata[index, 'Url'])
-    #             )
-    #           )
-    #         )
-    #       }
+          # details = function(index) {
+          #   div(
+          #     style = "padding: 15px; border: 1px solid #ddd; margin: 10px 0;
+          #    background-color: #E0EEEE; border-radius: 10px; border-color: black;
+          #    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);",
+          # 
+          #     tags$h4(
+          #       strong("Details"),
+          #     ),
+          #     tags$p(
+          #       strong('Metric Name: '),
+          #       as.character(metadata[index, 'Metric']),
+          #     ),
+          #     tags$p(
+          #       strong('Definition: '),
+          #       as.character(metadata[index, 'Definition']),
+          #     ),
+          #     tags$p(
+          #       strong('Source: '),
+          #       as.character(metadata[index, 'Source'])
+          #     ),
+          #     tags$p(
+          #       strong('Latest Year: '),
+          #       as.character(metadata[index, 'Year'])
+          #     ),
+          #     # tags$p(
+          #     #   strong('Updates: '),
+          #     #   str_to_title(as.character(metadata[index, 'updates']))
+          #     # ),
+          #     tags$p(
+          #       strong('URL: '),
+          #       tags$a(
+          #         href = as.character(metadata[index, 'Url']),
+          #         target = '_blank',
+          #         as.character(metadata[index, 'Url'])
+          #       )
+          #     )
+          #   )
+          # }
     #     )
     #   )
     # })
